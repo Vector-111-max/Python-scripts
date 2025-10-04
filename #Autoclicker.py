b2103b7491b42f7804929518fa474d2e03d6b216
Vector-111-max <victorserrano1522@gmail.com>
2025-10-04 05:48:52 -0400

    Added the option to choose clicking side

--- #Autoclicker.py
+++ #Autoclicker.py
@@ -1,13 +1,21 @@
 #Autoclicker
+#Ctrl + C to shut down
 
 import time 
 import pyautogui
 
 def autoclicker(interval):
+    global click_select 
     while True:
-        pyautogui.click()
-        time.sleep(interval)
+        if click_select.lower() == "left" :
+            pyautogui.click()
+            time.sleep(interval)
+        elif click_select.lower() == "right" :
+            pyautogui.rightClick()
+            time.sleep(interval)
 
+click_select = input("Select click button (\"Right or Left\"): ")
 interval = float(input("Time between clicks: "))
+print("Autoclicking...")
 
 autoclicker(interval)
