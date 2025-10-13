def4794b99292d1ab5dd2731c8298f835371374b
Vector-111-max <victorserrano1522@gmail.com>
2025-10-13 16:47:29 -0400

    Update To do list.py

--- To do list.py
+++ To do list.py
@@ -1,58 +1,67 @@
 #Not finished
-
 #To do list
-#[10/12/25] Note to myself : gotta fix empty string bug (fixed)
+#[12/10/25] Note to myself : gotta fix empty string bug (fixed)
 
 to_do = []
-def generate_list():
+def generate_list(): #Asks constant input from the user until indicated to stop. Then appends it to the to_do list
     print("Type \"done\" when you finish")
 
-    while not any(item.lower() == "done" for item in to_do):
+    while not any(item.lower() == "done" for item in to_do): #Starts the loop and constantly checks if the word "done" has been submitted. It's also case insensitive
         usr_lst = input("Task: ")
         if usr_lst == "":
             print("Must enter a Task!")
-            to_do.append(usr_lst)
-            to_do.insert(0, "")
-            del to_do[-1]
-            del to_do[0]
         elif usr_lst in to_do:
             print("Task already in list!") 
         else:
             to_do.append(usr_lst)
     
-    if any(item.lower() == "done" for item in to_do):
+    if any(item.lower() == "done" for item in to_do): #Deletes the word "done" from the list 
         del to_do[-1]
 
-def check_list():
+def check_list(): #prints to_do list in descending format
      if to_do == []:
-         print("No tasks")
+         print("No tasks") #Just a reminder in case the list is empty
      else:
         for i in to_do:
             print(i)
 
-def mark_task():
-    pass
+def mark_task(): #Marks a task as done
+    mark = input("Select a task: ")
+    if mark in to_do:
+        for i in range(0, len(to_do) - 1):
+            if to_do[i] == mark :
+                to_do[i] = "✓" + mark
 
 def del_task():
+    if to_do == []:
+        print("No tasks!")
     while to_do != []:
         task = input("Delete task: ")  
-        if task not in to_do:
-            print(f"There's not a task named {task}!")
-        elif task in to_do:
-            to_do.remove(task)
-            print("Task deleted") 
+        if task.lower() == "done":
+            break
+        elif task not in to_do:
+            print(f"There's not a task named {task}!") 
         else:
-            print("No tasks!")
+            to_do.remove(task)
+            print("Task deleted")
+            if to_do == []:
+                print("All tasks deleted")
 
-#Note to myself: add exception for string and float input
 while True: 
-    action = int(input("\"1\" to generate list\n\"2\" to check list\n\"4\" to delete a task\n- "))
+    try:
+        action = int(input("\"1\" to generate list\n\"2\" to check list\n\"4\" to delete a task\n- "))
 
-    if action == 1:
-        generate_list()
-    elif action == 2:
-        check_list()
-    #elif action == 3:
-    elif action == 4:
-        del_task()
-    #elif action == 5:
+        if action == 1:
+            generate_list()
+        elif action == 2:
+            check_list()
+        elif action == 3:
+            mark_task()
+        elif action == 4:
+            del_task()
+        elif action == 5:
+            break
+        else:
+            print("Must enter a valid option")
+    except ValueError:
+        print("Must enter a valid option")
