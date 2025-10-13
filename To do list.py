a04c8512bd2b2b82c322347540fcd19cf9347513
Vector-111-max <victorserrano1522@gmail.com>
2025-10-12 23:51:56 -0400

    Update To do list.py

--- To do list.py
+++ To do list.py
@@ -1,7 +1,7 @@
 #Not finished
 
 #To do list
-#Note to myself : gotta fix empty string bug
+#[10/12/25] Note to myself : gotta fix empty string bug (fixed)
 
 to_do = []
 def generate_list():
@@ -11,14 +11,16 @@ def generate_list():
         usr_lst = input("Task: ")
         if usr_lst == "":
             print("Must enter a Task!")
+            to_do.append(usr_lst)
+            to_do.insert(0, "")
+            del to_do[-1]
+            del to_do[0]
         elif usr_lst in to_do:
             print("Task already in list!") 
-            del to_do[-1]
-        to_do.append(usr_lst)
+        else:
+            to_do.append(usr_lst)
     
-    if "" in to_do:
-        to_do.remove("")
-    elif any(item.lower() == "done" for item in to_do):
+    if any(item.lower() == "done" for item in to_do):
         del to_do[-1]
 
 def check_list():
@@ -27,3 +29,30 @@ def check_list():
      else:
         for i in to_do:
             print(i)
+
+def mark_task():
+    pass
+
+def del_task():
+    while to_do != []:
+        task = input("Delete task: ")  
+        if task not in to_do:
+            print(f"There's not a task named {task}!")
+        elif task in to_do:
+            to_do.remove(task)
+            print("Task deleted") 
+        else:
+            print("No tasks!")
+
+#Note to myself: add exception for string and float input
+while True: 
+    action = int(input("\"1\" to generate list\n\"2\" to check list\n\"4\" to delete a task\n- "))
+
+    if action == 1:
+        generate_list()
+    elif action == 2:
+        check_list()
+    #elif action == 3:
+    elif action == 4:
+        del_task()
+    #elif action == 5:
